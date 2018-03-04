2b70b8f9d8ab2e4cee2c941fc5ebe09dcde2c804
cagricakir <cagricakir995@gmail.com>
2018-03-04 11:05:52 +0300

    Button is configured to send SMS

--- app/src/main/java/codedbycagri/smsmanage/MainActivity.java
+++ app/src/main/java/codedbycagri/smsmanage/MainActivity.java
@@ -2,6 +2,9 @@ package codedbycagri.smsmanage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final 
+        final EditText phoneNo = (EditText)findViewById(R.id.editText);
+        final EditText message = (EditText)findViewById(R.id.editText2);
+
+        Button send = (Button)findViewById(R.id.button);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.telephony.SmsManager sms = android.telephony.SmsManager.getDefault();
+                sms.sendTextMessage(phoneNo.getText().toString(), null, message.getText().toString(), null, null);
+
+            }
+        });
     }
 }
